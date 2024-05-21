696994043c8ab35a23175eed46628af34c16573e
viox <m.d.mihailik@gmail.com>
2024-05-21 14:40:03 +0300

    feat: add lab6

---
+++ lab6/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/viox/Viox/university/4th-semester/OsLabs/lab6/src/helperStuff.c" "CMakeFiles/main.dir/src/helperStuff.c.o" "gcc" "CMakeFiles/main.dir/src/helperStuff.c.o.d"
+  "/home/viox/Viox/university/4th-semester/OsLabs/lab6/src/main.c" "CMakeFiles/main.dir/src/main.c.o" "gcc" "CMakeFiles/main.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

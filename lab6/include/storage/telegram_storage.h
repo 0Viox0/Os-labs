97a1f7b61bdcd6e0cb9f26733d93d861e2c4a1fe
viox <m.d.mihailik@gmail.com>
2024-05-21 23:48:00 +0300

    feat: create add file

--- lab6/include/storage/telegram_storage.h
+++ lab6/include/storage/telegram_storage.h
@@ -4,7 +4,7 @@
 #include <curl/curl.h>
 
 size_t discard_response(void* ptr, size_t size, size_t nmemb, void* userdata);
-// void send_telegramm_message(const char* text);
-void send_telegramm_message(const char* fileContent, const char* message);
+void send_telegramm_message(const char* fileContent, const char* message,
+                            const char* file_name);
 
 #endif

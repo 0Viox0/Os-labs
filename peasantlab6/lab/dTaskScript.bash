d88278e8c4b3b461562c2fe0263bb2d6553c39d3
viox <m.d.mihailik@gmail.com>
2024-05-25 00:00:31 +0300

    feat: finish first group of countings

--- peasantlab6/lab/dTaskScript.bash
+++ peasantlab6/lab/dTaskScript.bash
@@ -4,4 +4,4 @@ for ((j = 0; j < 10; j++)); do
     ({ time ./launchScript.bash "$1"; } 2>> ./output4/parallelHeheWhereNIs$1) &
 done
 
-echo launched 10 processes, maximum time to wait is $(($1 * 3 * 10)) seconds
+echo launched 10 processes, maximum time to wait is $(($1 * 10)) seconds
